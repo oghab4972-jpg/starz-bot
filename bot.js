35e68790aad2dee4afa03656fb58981fa82db607
oghab4972-jpg <oghab4972@gmail.com>
2026-09-16 21:24:07 +0330

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 10000;
 app.use(express.static(path.join(__dirname, 'public')));
 
 // اگر کسی آدرس اصلی را باز کرد، فایل index.html مینی‌اپ فرستاده شود نه متن ساده
-app.get('/', (req, res) => {
+app.get('/', (req, res) => {َ
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
